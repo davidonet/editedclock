99c0c15166bc1f6dd26429fb993b70e2b9921fb4
David Olivari <david.olivari@gmail.com>
2014-06-18 09:20:02 +0200

    reorganizing folders

---
+++ arduino/clockacq.ino
@@ -0,0 +1,48 @@
+// the setup routine runs once when you press reset:
+void setup() {
+  Serial.begin(115200);  
+  pinMode(11,INPUT);
+  digitalWrite(11,HIGH);
+}
+
+unsigned long t = 0;
+boolean ispushed=false;
+
+void loop() {
+  if(digitalRead(11)==LOW){
+    if(!ispushed){
+      unsigned long b = millis();
+      
+      ispushed = true;
+      if(t==0){
+        Serial.write(0);
+      }
+      else{
+        unsigned long r = b-t;
+        byte v=255;
+        if((r/10)<255)
+          v = r/10;
+        Serial.write(v);
+      }
+      t=b;
+    }
+  }
+  else
+  { 
+    digitalWrite(13,LOW); 
+    ispushed=false;
+  }
+    if (Serial.available()) {
+    // read the most recent byte (which will be from 0 to 255):
+    if(0 == Serial.read()){
+      t=0;
+      digitalWrite(13,HIGH);
+      delay(200);
+    }
+  }
+}
+
+
+
+
+
